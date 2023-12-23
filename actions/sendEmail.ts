e513d4be1674f032902992c59d52d80e1cf27bb9
Yi Yang <yiyang@Yis-MacBook-Pro.domain>
2023-12-22 19:30:57 -0800

    test email

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -27,8 +27,8 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: "bytegrad@gmail.com",
-      subject: "Message from contact form",
+      to: "yiyangseattle@gmail.com",
+      subject: "Message from contact form of Angel portfolio",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
         message: message,
--- components/contact.tsx
+++ components/contact.tsx
@@ -30,8 +30,8 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:example@gmail.com">
-          example@gmail.com
+        <a className="underline" href="mailto:angelshenptl@gmail.com">
+          angelshenptl@gmail.com
         </a>{" "}
         or through this form.
       </p>
